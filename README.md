6e329ceaa54c47ce19db4cea4fdb20fbbb614178
George Lu <gylu88@gmail.com>
2016-04-04 08:01:06 +0800

    Outline done. Began coding

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # insight_coding_challenge
-the solution is in src/average_degree.py
-That file already includes the pacakges it needs (import sys, import json, import os)
+The solution file to this coding challenge is src/average_degree.py. That file already imports all the pacakges it needs. The following packages are used/imported:
+
+import sys - for reading the arugments of the run.sh command
+import json - for processing json
+import os - for checking if output.txt already exists, and deleting it if it does
+from itertools import combinations - used to run combinations (order doesn't matter), Taken from: https://rosettacode.org/wiki/Combinations#Python
+
+
+Overview of what the code does:
+#edge_list will be of the form: timestamp, node1, node2
+#1.     Get tweets:
+#1a     If it's a rate limiting message, ignore it
+
+#2.     Check timestamp and process accordingly:
+#2b     If timestamp is older than 60s, delete tweet, call calc_average_degree()
+#2c     If timestamp is not older than 60s, process the tweet
+
+#3.     Processing the tweet:
+#3a     If timestamp is newer than newest, update newest_timestamp value
+#3b     Clean the edge list: Delete edges that are older than 60 seconds
+#3c     If tweet has 2 or most hashtags, check and remove all duplicates
+#3d     If only 0 or 1 hashtag remains, discard
+
+#4      Create edge entries for each tweet that still has 2 or more distinct hashtags:
+#4a     Use the combination packcage that was imported. Eg: list(combinations(['hashtag1','hashtag2','hashtag3'],2)). This outputs a list of tuples.
+#4b     Sort each edge entry alphabetically so that we don't have the check the reverse. Do this by converting each tuple into a list and sorting
+
+#5      Insert each new edge entry into edge_list:
+#5a     Check that the edge (and the reverse) doesn't already exist, if it does, remove the older edge
+
+
+#7      call calc_average_degree()
+#7a     concatenate the 2 columns of nodes in the edge_list, and sum
+#7b     remove duplicates to get the number of nodes
+
--- src/average_degree.py
+++ src/average_degree.py
@@ -1,12 +1,8 @@
-# Things to be done:
-# 1. Build graph: e.g. list of edges with time stamp: "hashtags = [Spark , Apache],        created_at: Thu Mar 24 17:51:10 +0000 2016"
-## 1a. maintain list of valid nodes
-# 2. count degree for each edge for each node
-# 3. Find average
-
 import sys
 import os
 import json #use for json parser
+from itertools import combinations #used to run combinations (order doesn't matter)
+import time #needed to deal with timestamps
 
 #Note: From the way run.sh is called:
 #print(sys.argv[0]) #will be the name of this script: average_degree.py
@@ -19,40 +15,14 @@ import json #use for json parser
 # sys_argv2
 sys_argv1 = './tweet_input/tweets.txt'
 sys_argv2 = './tweet_output/output.txt'
+#todo:
+#remove tweet_input/tweets.txt from the gitignore
 
-#1. clear out old ./tweet_output/tweets.txt file
-if os.path.exists(sys_argv2):
-    os.remove(sys_argv2) #note that the location is based on where run.sh was called
-
-#Parse the input file
-#tweets_file = open(sys.argv[1])
-#tweets_json_format=json.load(tweets_file)
-
-#future optimization: this may be able to be optimized to process one-at-a-time as each json object line in the text file is read
-tweets = []
-for line in open(sys_argv1, 'r'):
-    line_json_parsed = json.loads(line)
-    if 'created_at' in line_json_parsed: #ignore the limit stuff
-        tweets.append(line_json_parsed)
-
-#example usage of the tweets array that examples the 248th tweet in the file
-tweets[248] # fetches a tweet
-tweets[248]['created_at'] # fetches timestamp of tweet
-tweets[248]['entities']['hashtags'] # fetches all hashtags of a tweet
-tweets[248]['entities']['hashtags'][1] # fetches individual hashtag of a tweet
-tweets[248]['entities']['hashtags'][1]['text'] # fetch individual hashtag text of a tweet
-len(tweets[248]['entities']['hashtags']) #tells you how many hashtags the tweet had
-
-
-#if number of hashtags is 0, just add the tweet
-#if number of hashtags is 1, remove the tag and add the tweet to the list (remove tag because list will be counted tags)
-
-#edge_list will be of the form: timestamp, number of hashtags, node1, node2
-
-#1.     Get tweets
+#edge_list will be of the form: timestamp, node1, node2
+#1.     Get tweets:
 #1a     If it's a rate limiting message, ignore it
 
-#2.     Check timestamp and process accordingly
+#2.     Check timestamp and:
 #2b     If timestamp is older than 60s, delete tweet, call calc_average_degree()
 #2c     If timestamp is not older than 60s, process the tweet
 
@@ -62,10 +32,12 @@ len(tweets[248]['entities']['hashtags']) #tells you how many hashtags the tweet 
 #3c     If tweet has 2 or most hashtags, check and remove all duplicates
 #3d     If only 0 or 1 hashtag remains, discard
 
-#4     Otherwise, If tweet has 2 or more distinct hashtags, create edge entry from each hashtag
+#4      Create edge entries for each tweet that still has 2 or more distinct hashtags:
+#4a     Use the combination packcage that was imported. Eg: list(combinations(['hashtag1','hashtag2','hashtag3'],2)). This outputs a list of tuples.
+#4b     Sort each edge entry alphabetically so that we don't have the check the reverse. Do this by converting each tuple into a list and sorting
 
-#5      Insert edge entry into edge_list
-#5a     Check that the edge and the reverse of that edge entry both don't already exist, if it does, remove the older edge
+#5      Insert each new edge entry into edge_list:
+#5a     Check that the edge (and the reverse) doesn't already exist, if it does, remove the older edge
 
 
 #7      call calc_average_degree()
@@ -75,18 +47,45 @@ len(tweets[248]['entities']['hashtags']) #tells you how many hashtags the tweet 
 
 
 
+#1. Get tweets
+#clear out old ./tweet_output/tweets.txt file
+if os.path.exists(sys_argv2):
+    os.remove(sys_argv2) #note that the location is based on where run.sh was called
 
-#get hashtags
-#remove repeat hashtags
-#graph[0]['timestamp'] = timestamp
-#graph[1]
+#1a. Ignore the rate limiting messages
+#future optimization: this may be able to be optimized to process one-at-a-time as each json object line in the text file is read
+tweets = []
+for line in open(sys_argv1, 'r'):
+    line_json_parsed = json.loads(line)
+    if 'created_at' in line_json_parsed: #ignore the limit stuff
+        tweets.append(line_json_parsed)
 
-# get_tweet
-# check_timestamp
-#     if (older than 60 sec from newest?)
-#         call throw out
-#     else
-#         newer than newest? 
-#             update newest
-#             run throw out oldest
-#         older than oldest? update oldest
+# #Example usage of the tweets array that examples the 248th tweet in the file
+# tweets[248] # fetches a tweet
+# tweets[248]['created_at'] # fetches timestamp of tweet
+# tweets[248]['entities']['hashtags'] # fetches all hashtags of a tweet
+# tweets[248]['entities']['hashtags'][1] # fetches individual hashtag of a tweet
+# tweets[248]['entities']['hashtags'][1]['text'] # fetch individual hashtag text of a tweet
+# len(tweets[248]['entities']['hashtags']) #tells you how many hashtags the tweet had
+
+newest_timestamp=0.00;
+#check timestamp
+
+for tweet in tweets:
+    tweetWithinTimeRange = checkTimeStamp_withinRange(tweet['created_at']); #note that this created_at field is always utc time
+    if (!tweetWithinTimeRange):
+        calc_average_degree();
+    else:
+
+
+def checkTimeStamp_withinRange( newTimeStampToBeChecked ):
+    #newTimeStampToBeChecked is something in the format of "Wed Aug 29 17:12:58 +0000 2012"
+    timestampToBeChecked_epoch_utc = time.mktime(time.strptime(created_at,"%a %b %d %H:%M:%S +0000 %Y")) #followed this http://stackoverflow.com/questions/18604755/twitter-created-at-convert-epoch-time-in-python
+    if timestampToBeChecked_epoch_utc < newest_timestamp -60:
+        return false
+    else if timestampToBeChecked_epoch_utc > newest_timestamp:
+        newest_timestamp=timestampToBeChecked_epoch_utc;
+    return true
+ 
+def calc_average_degree():
+    return true
